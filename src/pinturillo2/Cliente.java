d53976a17cfd40b273312b0efde5677c656cee65
KevinnMatoss <kmatoscerqueira@gmail.com>
2019-06-28 10:10:12 +0100

    add Cliente Class
    Signed-off-by: KevinnMatoss <kmatoscerqueira@gmail.com>

---
+++ src/pinturillo2/Cliente.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pinturillo2;
+
+import java.io.*;
+import java.net.*;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author kevin
+ */
+public class Cliente extends Thread {
+    InetAddress ip;
+    Socket s;
+    DataInputStream dis;
+    DataOutputStream dos;
+    Consumer<String> onNewFromServer;
+
+    public Thread sendMessage = new Thread(new Runnable(){
+        @Override
+        public void run() {
+            while(true){
+                String msg = Le.umaString();
+                try {
+                    dos.writeUTF(msg);
+                    if(msg.equals("logout")){
+                        dos.close();
+                        break;
+                    }
+                } catch (IOException e) {
+                }
+            }
+        }
+
+    });
+
+    public Thread readMessage = new Thread(new Runnable(){
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    String recebido = dis.readUTF();
+                    onNewFromServer.accept(recebido);
+
+                } catch (Exception e) {
+                }      
+            }
+        }           
+    });
+    
+    public Cliente(Consumer<String> onNewFromServer) throws UnknownHostException, IOException {
+        this.ip = InetAddress.getByName("localhost");
+        this.s = new Socket(ip, 1234);
+        this.dis = new DataInputStream(s.getInputStream());
+        this.dos = new DataOutputStream(s.getOutputStream());
+        this.onNewFromServer = onNewFromServer;
+        
+        this.sendMessage.setDaemon(true);
+        this.readMessage.setDaemon(true);
+        this.sendMessage.start();
+        this.readMessage.start();
+    }
+    
+}
